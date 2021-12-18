07ad7db8dd90f3722574dc64e05bc3d02a5d4614
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-18 19:21:24 +0200

    finances db

---
+++ finances_db_creation.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS finance; 
+CREATE DATABASE finance;
+
+USE finance;
+
+CREATE TABLE budget(
+	code_name varchar(50) PRIMARY KEY,
+    daily_limit integer,
+    mounth_limit integer
+);
+    
+CREATE TABLE category(
+	code_name varchar(100) PRIMARY KEY,
+    category_name varchar(50),
+    aliases_ text
+);
+
+CREATE TABLE expenses(
+	expense_id integer NOT NULL,
+    amount integer,
+    date_time datetime, 
+    category varchar(100),
+    message_text_ text, 
+    CHECK (amount > -1),
+    FOREIGN KEY (category) REFERENCES category(code_name)
+);
+
+CREATE TABLE incomes(
+	income_id integer NOT NULL,
+    amount integer,
+    date_time datetime,
+    category varchar(100),
+    message_text_ text,
+    CHECK (amount > -1),
+    FOREIGN KEY (category) REFERENCES category(code_name)
+);
+
+
+
