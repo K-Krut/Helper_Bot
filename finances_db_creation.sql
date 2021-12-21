81572cbad73b8cb1f2309e2d422a62fff5dac086
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-22 00:21:31 +0200

    Update finances_db_creation.sql

--- finances_db_creation.sql
+++ finances_db_creation.sql
@@ -1,19 +1,32 @@
 -- DROP DATABASE IF EXISTS finance; 
 -- CREATE DATABASE finance;
-DROP TABLE IF EXISTS budget;
 
 USE finance;
 
-CREATE TABLE budget (
-	code_name varchar(50) PRIMARY KEY,
-    daily_limit integer DEFAULT 0,
-    month_limit integer DEFAULT 0
+CREATE TABLE users(
+	id VARCHAR(50) NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) DEFAULT NULL,
+    last_name VARCHAR(50) DEFAULT NULL,
+    username VARCHAR(50) DEFAULT NULL,
+    UNIQUE KEY (username)
 );
-    
+
+CREATE TABLE budget (
+	code_name varchar(50) DEFAULT 'general',
+    daily_limit integer DEFAULT 0,
+    month_limit integer DEFAULT 0,
+    user_id varchar(50) NOT NULL,
+    PRIMARY KEY (code_name, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 CREATE TABLE category(
-	code_name varchar(100) PRIMARY KEY,
+	code_name varchar(100),
     category_name varchar(50),
-    aliases_ text
+    aliases_ text,
+	user_id varchar(50) NOT NULL,
+    PRIMARY KEY(code_name, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE expenses (
@@ -21,7 +34,9 @@ CREATE TABLE expenses (
     amount integer,
     date_time datetime, 
     category varchar(100),
+	user_id varchar(50) NOT NULL,
     PRIMARY KEY(expense_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category) REFERENCES category(code_name)
 );
 
@@ -30,9 +45,9 @@ CREATE TABLE incomes (
     amount integer,
     date_time datetime,
     category varchar(100),
+	user_id varchar(50) NOT NULL,
     PRIMARY KEY(income_id), 
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category) REFERENCES category(code_name)
 );
-
-
 
