f87741e9bc6214c662c65df35005afbdac7a5543
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-21 23:32:30 +0200

    Update imports.py

--- imports.py
+++ imports.py
@@ -12,11 +12,3 @@ import time
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-
-connection = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="Varta4899",
-    db='finance'
-)
