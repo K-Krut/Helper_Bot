1852abdacdb115f6549961b1f1e80df77305ceb8
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-12-22 21:14:05 +0200

    databases functional editing

--- databases.py
+++ databases.py
@@ -2,8 +2,10 @@ from imports import connection
 
 
 def insert(table, column_values):
+    print(column_values)
     columns = ', '.join(column_values.keys())
     values = [tuple(i for i in column_values.values())]
+    print(f'VALUES {values} COL {column_values}')
     placeholders = '%s' + ', %s' * (len(column_values) - 1)
     with connection.cursor() as cursor:
         cursor.executemany(f'INSERT INTO {table} ({columns}) VALUES ({placeholders})', values)
